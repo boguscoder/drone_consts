e6296ff092d4295b9a7c43ca7da0dd486fc5ce69
Oleg Bogdanov <boguscoder@gmail.com>
2026-08-23 19:42:46 -0700

    added telemetry state

--- src/lib.rs
+++ src/lib.rs
@@ -15,6 +15,32 @@ pub mod telemetry {
         Pid,
         Mix,
         Dshot,
-        AdHoc,
+        Dump,
+    }
+
+    #[derive(Copy, Clone, PartialEq, Eq, TryFromPrimitive)]
+    #[repr(u8)]
+    pub enum Mode {
+        Stream = 0,
+        Store,
+    }
+
+    #[derive(Copy, Clone)]
+    pub struct TelemetryState {
+        pub mode: Mode,
+        pub category: Category,
+    }
+
+    impl TelemetryState {
+        pub const fn to_u16(self) -> u16 {
+            ((self.mode as u16) << 8) | (self.category as u16)
+        }
+
+        pub fn from_u16(val: u16) -> Self {
+            let mode = Mode::try_from((val >> 8) as u8).unwrap_or(Mode::Stream);
+            let category = Category::try_from((val & 0xFF) as u8).unwrap_or(Category::None);
+
+            Self { mode, category }
+        }
     }
 }
