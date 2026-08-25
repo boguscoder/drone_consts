9e195bd31f28d86231ae4e3d2be2306d6e47593d
Oleg Bogdanov <boguscoder@gmail.com>
2026-08-24 19:21:55 -0700

    actually, no need for state

--- src/lib.rs
+++ src/lib.rs
@@ -24,23 +24,4 @@ pub mod telemetry {
         Stream = 0,
         Store,
     }
-
-    #[derive(Copy, Clone)]
-    pub struct TelemetryState {
-        pub mode: Mode,
-        pub category: Category,
-    }
-
-    impl TelemetryState {
-        pub const fn to_u16(self) -> u16 {
-            ((self.mode as u16) << 8) | (self.category as u16)
-        }
-
-        pub fn from_u16(val: u16) -> Self {
-            let mode = Mode::try_from((val >> 8) as u8).unwrap_or(Mode::Stream);
-            let category = Category::try_from((val & 0xFF) as u8).unwrap_or(Category::None);
-
-            Self { mode, category }
-        }
-    }
 }
