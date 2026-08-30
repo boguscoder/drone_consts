4039cea84afc5e6906ac07e947092b52e91a17dc
Oleg Bogdanov <boguscoder@gmail.com>
2026-08-30 12:03:10 -0700

    Split dump into separate command

--- src/lib.rs
+++ src/lib.rs
@@ -4,9 +4,9 @@ pub mod telemetry {
     use num_enum::TryFromPrimitive;
     use strum_macros::{AsRefStr, EnumIter};
 
-    #[derive(Debug, EnumIter, AsRefStr, PartialEq, Clone, Copy, TryFromPrimitive)]
+    #[derive(Debug, EnumIter, AsRefStr, Eq, PartialEq, Clone, Copy, TryFromPrimitive)]
     #[repr(u8)]
-    pub enum Category {
+    pub enum Mode {
         None = 0,
         Imu,
         Baro,
@@ -15,13 +15,40 @@ pub mod telemetry {
         Pid,
         Mix,
         Dshot,
-        Dump,
     }
 
-    #[derive(Copy, Clone, PartialEq, Eq, TryFromPrimitive)]
-    #[repr(u8)]
-    pub enum Mode {
-        Stream = 0,
-        Store,
+    #[derive(Copy, Clone, PartialEq, Eq, Debug)]
+    pub enum Command {
+        SetTelemetryMode(Mode),
+        DumpFlash,
+    }
+
+    impl From<Command> for u8 {
+        fn from(val: Command) -> u8 {
+            match val {
+                Command::SetTelemetryMode(cat) => (cat as u8) << 4,
+                Command::DumpFlash => 0x01,
+            }
+        }
+    }
+
+    impl TryFrom<u8> for Command {
+        type Error = &'static str;
+
+        fn try_from(val: u8) -> Result<Self, Self::Error> {
+            match val & 0x0F {
+                0x00 => {
+                    let cat = Mode::try_from(val >> 4).map_err(|_| "Invalid Category")?;
+                    Ok(Command::SetTelemetryMode(cat))
+                }
+                0x01 => {
+                    if (val >> 4) != 0 {
+                        return Err("DumpFlash expects no payload");
+                    }
+                    Ok(Command::DumpFlash)
+                }
+                _ => Err("Unknown Command ID"),
+            }
+        }
     }
 }
